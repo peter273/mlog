3bfc636edea73fb779f3d474c2e7f2c1ac0bc147
peter273 <bsheep273@gmail.com>
2017-03-07 20:45:36 +0800

    minor commit

--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup,Executable
-
-setup(
-    name="MineLog",
-    version="1.0",
-    description = "Mine Equipment Evaluation",
-    executables=[Executable("main.py",base="Win32GUI")])
