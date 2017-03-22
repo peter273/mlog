f269db486636946ca27787c78b7430c5196c9459
peter273 <bsheep273@gmail.com>
2017-03-22 08:24:21 +0800

    view data

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'main.ui'
-#
-# Created: Sun Mar 19 18:46:29 2017
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
 import sys
 from PySide import QtCore, QtGui
 
-#Mine Log 
-# import MineLog as ml
 from MineLog import Equipment,ShiftFile,mload,oeeEquipmentPlot,Moving_AveragePlot 
 
 import os
@@ -26,39 +18,96 @@ class NewEqDialog(QtGui.QDialog,Ui_Dialog):
         name=self.eqName_lineedit.text()
         eqtype= self.eqtype_combobox.currentText()
         return (str(name).strip(),str(eqtype))
+class EqItemGui:
+    def __init__(self,eq,eq_gui):
+        self.eq=eq
+        self.eq_gui
 
 from mlogUI import mwindow
 class MainUI(mwindow.Ui_MainWindow):
     def setupUi(self,MainWindow):
         super(MainUI,self).setupUi(MainWindow)
-        self.ext_init()
-    def ext_init(self):
+        dirname=os.path.dirname(__file__)
+
+        icon_path={
+                'new':['plus.png',self.new_toolbtn],
+                'delete':['cancel-2.png',self.delete_toolbtn],
+                'addshift':['copy.png',self.addshift_toolbtn],
+                'info':['information.png',self.info_toolbtn],
+                'plot':['next-1.png',self.plot_toolbtn]}
+
+        for i in icon_path:
+            icon_path[i][0]=os.path.join(dirname,"mlogUI/flaticons/icons/png/"+icon_path[i][0])
+            icon = QtGui.QIcon()
+            icon.addPixmap(QtGui.QPixmap(icon_path[i][0]), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            icon_path[i][1].setIcon(icon)
+            icon_path[i][1].setIconSize(QtCore.QSize(30, 30))
+
         self.plot_toolbtn.clicked.connect(self.plt_btn_clicked)
         self.shovel_listwidget.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+        self.shovel_listwidget.itemSelectionChanged.connect(self.itemSelectionChanged)
         self.truck_listwidget.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
-        self.shovel_listwidget.currentItemChanged.connect(self.item_click)
         self.ma_checkbox.stateChanged.connect(self.maspinner_changestate)
         self.ma_spinBox.setEnabled(False)
         self.get_initial_equipment_list()
 
-        # for i in range(1,21):
-        #     name = "Shovel{0}".format(i)
-        #     shovel_item=QtGui.QListWidgetItem()
-        #     shovel_item.setText(name)
-        #     self.shovel_listwidget.addItem(shovel_item)
-        #     cut_data=Data(i)
-        #     shovel_item.setData(QtCore.Qt.UserRole,cut_data)
-        # for i in range(1,21):
-        #     self.truck_listwidget.addItem("Truck{0}".format(i))
-    # Change Spinner state when Moving Average check box state changes
+        self.display_headers()
+        self.data_to_display=[]
+    # Display Headers
+    def display_headers(self):
+        header=QtGui.QTreeWidgetItem([
+            "Name",
+            "OEE",
+            "Availability",
+            "Utilization",
+            "Efficiency"])
+        self.data_listwidget.setHeaderItem(header)
+
+    # Change Moving Average Spinner state when Moving Average check box state changes
     def maspinner_changestate(self):
         if self.ma_checkbox.isChecked():
             self.ma_spinBox.setEnabled(True)
         else:
             self.ma_spinBox.setEnabled(False)
-    def item_click(self,arg=None):
+
+    def itemSelectionChanged(self):
         #TODO change data to view upon change equipment
-        pass
+        
+        eq = self.chosen_equipment()
+        eqpment = [i.data(QtCore.Qt.UserRole) for i in eq]
+        data_frame=self.chosen_dataframe()
+
+        # eqlist=self.eq_tab.currentIndex()
+        # if eqlist==0: eq_listwidget=self.shovel_listwidget
+        # elif eqlist==1: eq_listwidget=self.truck_listwidget
+
+        # eqpment= [i.data(
+        # for i in range(eq_listwidget.count()): 
+        #     print(eq_listwidget.item(i))
+
+        header=QtGui.QTreeWidgetItem([
+            "Name",
+            "OEE",
+            "Availability",
+            "Utilization",
+            "Efficiency"])
+        main_=self.data_listwidget
+        main_.clear()
+        main_.setHeaderItem(header)
+        for i in eqpment:
+            eq_item=QtGui.QTreeWidgetItem(main_,[i.Name])
+            eq_item.setData(2,QtCore.Qt.UserRole,i)
+            attributes=["OEE",
+                    "Availability",
+                    "Utilization",
+                    "Efficiency"]
+            for ctr in range(len(i.Data)):
+                Date=str(i.Data.iloc[ctr].Date.date())
+                Shift=i.Data.iloc[ctr].Shift
+                param=[str(getattr(i.Data.iloc[ctr],temp)) for temp in attributes]
+                shift_item=QtGui.QTreeWidgetItem(eq_item,[Date+" Shift "+Shift,
+                    *param])
+                    
 
     # Return the Time Frame to be Plotted
     def chosen_dataframe(self):
@@ -89,7 +138,7 @@ class MainUI(mwindow.Ui_MainWindow):
             if i.checkState():
                 params.append(i.text())
         return params
-
+    # Plotting of Data 
     def plt_btn_clicked(self):
         eq = self.chosen_equipment()
         eqpment = [i.data(QtCore.Qt.UserRole) for i in eq]
@@ -103,7 +152,6 @@ class MainUI(mwindow.Ui_MainWindow):
         if moving_ave_on and eqpment and params:
             Moving_AveragePlot(eqpment,params,data_frame,interval=interval)
         # TODO add dialog for no chosen parameter/equipment
-
 
     def get_initial_equipment_list(self):
         dirpath=os.path.dirname(__file__)
@@ -138,6 +186,7 @@ class MineLog(QtGui.QMainWindow):
         self.ui.delete_toolbtn.clicked.connect(self.func_deleq)
         #TODO check if Equipment folder exists
 
+    # Delete Equipment Function
     def func_deleq(self):
         x=self.ui.chosen_equipment()
         if len(x)>0:
@@ -150,7 +199,23 @@ class MineLog(QtGui.QMainWindow):
             msg.setText('No Chosen Equipment')
             msg.setWindowTitle("Warning")
             msg.show()
+    def delete_equipment(self):
+        dirpath=os.path.dirname(__file__)
+        filepath = os.path.join(dirpath,'Equipment')
+        if not os.path.exists(filepath):
+            os.mkdir(filepath)
 
+        eqlist=self.ui.eq_tab.currentIndex()
+        if eqlist==0: eq_listwidget=self.ui.shovel_listwidget
+        elif eqlist==1: eq_listwidget=self.ui.truck_listwidget
+        
+        x = eq_listwidget.selectedItems()
+        for i in x:
+            eq_listwidget.takeItem(eq_listwidget.row(i))
+            g = i.data(QtCore.Qt.UserRole)
+            os.remove(os.path.join(filepath,g.Name+".mlog"))
+
+    # Add Shift Function
     def func_addshift(self):
         # TODO add shift option to add from folder
         dirpath=os.path.dirname(__file__)
@@ -180,7 +245,7 @@ class MineLog(QtGui.QMainWindow):
             msg.setText('Choose only one Equipment')
             msg.setWindowTitle("Warning")
             msg.show()
-
+    # Opens Dialog for Creating New Equipment
     def new_buttonClicked(self):
         dialog = NewEqDialog(self)
         if dialog.exec_():
@@ -192,22 +257,7 @@ class MineLog(QtGui.QMainWindow):
                 msg.show()
             else: 
                 self.create_equipment(eq_name,eq_type)
-    def delete_equipment(self):
-        dirpath=os.path.dirname(__file__)
-        filepath = os.path.join(dirpath,'Equipment')
-        if not os.path.exists(filepath):
-            os.mkdir(filepath)
-
-        eqlist=self.ui.eq_tab.currentIndex()
-        if eqlist==0: eq_listwidget=self.ui.shovel_listwidget
-        elif eqlist==1: eq_listwidget=self.ui.truck_listwidget
-        
-        x = eq_listwidget.selectedItems()
-        for i in x:
-            eq_listwidget.takeItem(eq_listwidget.row(i))
-            g = i.data(QtCore.Qt.UserRole)
-            os.remove(os.path.join(filepath,g.Name+".mlog"))
-
+    # Function for creating New Equipment
     def create_equipment(self,name,eq_type):
         #TODO Check if Equipment Name Already exist
         import os
